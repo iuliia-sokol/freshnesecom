4b07ebaaf4b03e8eb2b468f15ef5252d35ef1810
Iuliia Sokol <avare779@gmail.com>
2022-09-12 20:09:20 +0300

    Polyfill for forEach added to JS for select-panel

--- search_select.js
+++ search_select.js
@@ -1,3 +1,12 @@
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
 const selectBtn = document.querySelector(".select__btn");
 const selectList = document.querySelector(".select__list");
 const selectOptions = selectList.querySelectorAll(".select__option");

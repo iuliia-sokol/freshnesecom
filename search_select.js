29bb2426166fea0c45b565fc2741e82d40a36cb3
Iuliia Sokol <avare779@gmail.com>
2022-09-12 19:40:14 +0300

    select JS finilized

--- search_select.js
+++ search_select.js
@@ -1,15 +1,37 @@
-document.querySelector(".select__btn").addEventListener("click", function () {
-  document
-    .querySelector(".select__list")
-    .classList.toggle("select__list--visible");
+const selectBtn = document.querySelector(".select__btn");
+const selectList = document.querySelector(".select__list");
+const selectOptions = selectList.querySelectorAll(".select__option");
+const searchInput = document.querySelector(".search__input");
+const searchForm = document.querySelector(".search__form");
+const searchBtn = document.querySelector(".search__btn");
+
+selectBtn.addEventListener("click", function () {
+  selectList.classList.toggle("select__list--visible");
+  searchForm.classList.toggle("search__form--selected");
 });
 
-document.querySelectorAll(".select__option").forEach(function (listItem) {
-  listItem.addEventListener("click", function () {
-    document.querySelector(".select__btn").innerText = this.innerText;
-    document
-      .querySelector(".select__list")
-      .classList.remove("select__list--visible");
-    document.querySelector(".search__input").value = this.dataset.value;
+selectOptions.forEach(function (listItem) {
+  listItem.addEventListener("click", function (e) {
+    e.stopPropagation();
+    selectBtn.innerText = this.innerText;
+    selectList.classList.remove("select__list--visible");
+    searchInput.value = this.dataset.value;
+    searchForm.classList.remove("search__form--selected");
   });
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target !== selectBtn) {
+    selectList.classList.remove("select__list--visible");
+    searchForm.classList.remove("search__form--selected");
+    // selectBtn.innerText = "All categories";
+    // searchInput.value = null;
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Tab" || e.key === "Escape") {
+    selectList.classList.remove("select__list--visible");
+    searchForm.classList.remove("search__form--selected");
+  }
 });
